fcb664bf8ad63ace8971e3e77a00bd7d70499419
huy <nguyenlehuyuit@gmail.com>
2023-01-12 09:13:43 +0700

    Add archive channel feature

--- app/Http/Controllers/ThreadController.php
+++ app/Http/Controllers/ThreadController.php
@@ -45,7 +45,7 @@ class ThreadController extends Controller
         $validated = $request->validate([
             'title' => ['required', new CheckSpam(Thread::class, 'title')],
             'body' => ['required', new CheckSpam(Thread::class, 'body')],
-            'channel_id' => ['required', Rule::exists('channels', 'id')]
+            'channel_id' => ['required', Rule::exists('channels', 'id')->where('is_archive', false)]
         ]);
 
         $thread = $request->user()->threads()->create($validated);
